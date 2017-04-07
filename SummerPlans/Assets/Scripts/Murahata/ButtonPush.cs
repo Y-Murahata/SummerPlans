e997b5b1da53c8d8b46387b6f5f2cc77c757bcb0
Yuuma Murahata <spartan0606my@gmail.com>
2017-04-07 13:42:25 +0900

    仮タイトルとボタンを追加

---
+++ SummerPlans/Assets/Scripts/Murahata/ButtonPush.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonPush : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //ボタンが押されたらシーンを切り替える処理
+    public void ButtonPushed()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+}

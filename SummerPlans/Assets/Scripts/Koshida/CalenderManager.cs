e73639e8e914adac28771e7ccb222ae544505e2c
unknown <s162068>
2017-04-08 00:53:43 +0900

    １Pがカレンダーの範囲選択が可能に

---
+++ SummerPlans/Assets/Scripts/Koshida/CalenderManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalenderManager : MonoBehaviour {
+
+    int _day_num = 31;
+
+    GameObject[] _1Pdays;
+    // Use this for initialization
+    void Start () {
+        _1Pdays = new GameObject[_day_num];
+
+        for (int i = 0; i < _day_num; i++)
+            _1Pdays[i] = GameObject.Find((i).ToString());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void ChangeColor(int first_num,int second_num)
+    {
+        if(first_num < second_num)
+        {
+            for (int i = first_num; i<= second_num;i++)
+            {
+                _1Pdays[i].GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.5f, 0.5f, 1.0f);
+                Debug.Log(first_num);
+            }
+        }
+        else
+        {
+            for (int i = first_num; i >= second_num; i--)
+            {
+                _1Pdays[i].GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.5f, 0.5f, 1.0f);
+                Debug.Log(first_num);
+            }
+        }
+    }
+}
---
+++ SummerPlans/Assets/Scripts/Koshida/ClickDay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDay : MonoBehaviour {
+
+    int _first_num,_second_num;
+    GameObject cm_obj;
+
+	// Use this for initialization
+	void Start () {
+        _first_num = -1;
+        _second_num = -1;
+        cm_obj = GameObject.Find("CalendarManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //左クリックされたとき
+		if(Input.GetMouseButtonDown(0))
+        {
+            //マウスポインタの位置をワールド座標に変換
+            Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(point, Vector2.zero);
+
+            if (hit)
+            {
+                if(_first_num < 0)
+                {
+                    _first_num = int.Parse(hit.collider.name);
+                    GameObject obj = hit.collider.gameObject;
+                    obj.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.5f, 0.5f, 1.0f);
+                }
+                else
+                {
+                    CalenderManager cm = cm_obj.GetComponent<CalenderManager>();
+
+                    _second_num = int.Parse(hit.collider.name);
+                    cm.ChangeColor(_first_num, _second_num);
+                    _first_num = -1;
+                    _second_num = -1;
+                }
+            }
+        }
+    }
+}

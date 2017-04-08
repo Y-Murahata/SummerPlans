5313c74baecb849dade6a389ebc627a0cb82401f
unknown <s162068>
2017-04-08 22:53:44 +0900

    予定のキャンセル

--- SummerPlans/Assets/Scripts/Koshida/CalenderManager.cs
+++ SummerPlans/Assets/Scripts/Koshida/CalenderManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CalenderManager : MonoBehaviour {
 
     int _day_num = 31;
+    int first_num = -1;
+    int second_num = -1;
+    static int select_num = 0;
 
     GameObject[] _1Pday_obj;
 
@@ -24,58 +27,140 @@ public class CalenderManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
-	}
-
-    public void SelectDay(int first_num,int second_num)
-    {
-        //選択範囲が一週間以上ならば
-        if (Mathf.Abs(first_num - second_num) > 7)
+        for (int i = 0; i < _day_num; i++)
         {
-            _1Pday_state[first_num] = 0;
+            switch (_1Pday_state[i])
+            {
+                case 0:
+                    _1Pday_obj[i].GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                    break;
 
-            ChangeColor(first_num);            
-            return;
+                case 1:
+                case 2:
+                case 3:
+                    _1Pday_obj[i].GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.5f, 0.5f, 1.0f);
+                    break;
+            }
         }
+    }
+
+    //日付の選択
+    public void SelectDay(int day_num)
+    {
+        if (select_num < 3)
+        {
+
+            if (first_num < 0)
+            {
+                if (IsSelected(day_num))
+                {
+                    return;
+                }
+
+                _1Pday_state[day_num] = select_num + 1;
+
+                first_num = day_num;
+            }
+            else
+            {
+                if (IsSelected(day_num))
+                {
+                    _1Pday_state[first_num] = 0;
+                    first_num = -1;
+                    second_num = -1;
+                    return;
+                }
+
+                second_num = day_num;
+
+                //選択範囲が一週間以上ならば
+                if (Mathf.Abs(first_num - second_num) > 7)
+                {
+                    _1Pday_state[first_num] = 0;
+                    first_num = -1;
+                    second_num = -1;
+                    return;
+                }
+
+                EnterPlan(first_num, second_num);
+                first_num = -1;
+                second_num = -1;
+                select_num++;
+            }
+        }
+    }
+
+
+
+    //予定の確定
+    void EnterPlan(int first_num,int second_num)
+    {
 
         if (first_num < second_num)
         {
             for (int i = first_num; i<= second_num;i++)
             {
-                _1Pday_state[i] = 1;
-                ChangeColor(i);
+                _1Pday_state[i] = select_num + 1;
             }
         }
         else
         {
             for (int i = first_num; i >= second_num; i--)
             {
-                _1Pday_state[i] = 1;
-                ChangeColor(i);
+                _1Pday_state[i] = select_num + 1;
             }
         }
     }
 
-    void ChangeColor(int day_num)
+    public void CancelPlan(int day_num)
     {
-        switch(_1Pday_state[day_num])
+        if (_1Pday_state[day_num] == 0)
         {
-            case 0:
-                _1Pday_obj[day_num].GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-                break;
-
-            case 1:
-                _1Pday_obj[day_num].GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.5f, 0.5f, 1.0f);
-                break;
+            return;
         }
+
+        int select_day_state = _1Pday_state[day_num];
+
+        while (_1Pday_state[day_num] == select_day_state)
+        {
+            day_num++;
+            if (day_num >= _day_num)
+            {
+                day_num = _day_num;
+                break;
+            }
+        }
+
+        day_num--;
+        int top_day = day_num;
+
+        while (_1Pday_state[day_num] == select_day_state)
+        {
+            day_num--;
+            if (day_num < 0)
+            {
+                day_num = 0;
+                break;
+            }
+        }
+
+        day_num++;
+        int bottom_day = day_num - 1;
+
+        for (int i = bottom_day; i <= top_day; i++)
+        {
+            _1Pday_state[i] = 0;
+        }
+
+        select_num--;
     }
+
 
     public bool IsSelected(int day_num)
     {
         //選んだ日付が選択済みだったら
         if(_1Pday_state[day_num] > 0)
         {
-            Debug.Log(_1Pday_state[day_num]);
             return true;
         }
 
--- SummerPlans/Assets/Scripts/Koshida/ClickDay.cs
+++ SummerPlans/Assets/Scripts/Koshida/ClickDay.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class ClickDay : MonoBehaviour {
 
-    int _first_num,_second_num;
     GameObject cm_obj;
 
 	// Use this for initialization
 	void Start () {
-        _first_num = -1;
-        _second_num = -1;
         cm_obj = GameObject.Find("CalendarManager");
 	}
 	
@@ -29,26 +26,23 @@ public class ClickDay : MonoBehaviour {
             {
                 CalenderManager cm = cm_obj.GetComponent<CalenderManager>();
 
-                //一回目の選択がまだだったら
-                if (_first_num < 0)
-                {
-                    _first_num = int.Parse(hit.collider.name);
-                    if(cm.IsSelected(_first_num))
-                    {
-                        _first_num = -1;
-                        return;
-                    }
-                    GameObject obj = hit.collider.gameObject;
-                    obj.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.5f, 0.5f, 1.0f);
-                }
-                else
-                {
-                    _second_num = int.Parse(hit.collider.name);
-                    
-                    cm.SelectDay(_first_num, _second_num);
-                    _first_num = -1;
-                    _second_num = -1;
-                }
+                cm.SelectDay(int.Parse(hit.collider.name));
+            }
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            //マウスポインタの位置をワールド座標に変換
+            Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(point, Vector2.zero);
+
+            //オブジェクトに当たった時
+            if (hit)
+            {
+                CalenderManager cm = cm_obj.GetComponent<CalenderManager>();
+
+                cm.CancelPlan(int.Parse(hit.collider.name));
             }
         }
     }
